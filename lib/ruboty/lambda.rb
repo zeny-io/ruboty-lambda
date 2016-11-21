4477b5225f071dec48ad07c156314758ad6b7c38
Sho Kusano <rosylilly@aduca.org>
2016-11-21 18:37:44 +0900

    Init

---
+++ lib/ruboty/lambda.rb
@@ -0,0 +1,7 @@
+require "ruboty/lambda/version"
+
+module Ruboty
+  module Lambda
+    # Your code goes here...
+  end
+end
---
+++ lib/ruboty/lambda/version.rb
@@ -0,0 +1,5 @@
+module Ruboty
+  module Lambda
+    VERSION = "0.1.0"
+  end
+end
